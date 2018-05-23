0610e79a23e42e0c209034f6a3432678d0eda03b
jonopens <jonopenshaw@gmail.com>
2018-05-23 18:37:36 -0400

    added basic functionality for single css file

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 This started as an interest in adding an inverted color scheme to my personal site [jonopens.com](http://www.jonopens.com) and then grew into a desire to try creating a node package.
 
+The goal of css-color-inverter is to generate a new css file from an existing one with inverted hexadecimal color values.
+
+This is currently an active project and is in no way warranted to work for production purposes at this time.
+
 ## Usage
 
-Coming...
+Super simple currently. Navigate to the directory of the `.css` file of which you'd like to invert the color scheme and run `cssinvert <filename>`.
+
+## Planned Features
+
+- tests
+- invert multiple files with single command
+
+##
--- index.js
+++ index.js
@@ -1,4 +1,62 @@
+#!/usr/bin/env node --harmony
+
 const program = require('commander');
 const fs = require('fs');
 
-console.log('hi there.')
+// reference objects
+const hexIndexReference = {
+  '0': 0,'1': 1,'2': 2,'3': 3,'4': 4,'5': 5,'6': 6,'7': 7,'8': 8,'9': 9,'A': 10,'B': 11,'C': 12,'D': 13,'E': 14,'F': 15
+};
+const hexValueArray = Object.keys(hexIndexReference).sort(); // sort in case order not preserved
+const hexValueRegex = /#[a-fA-F0-9]{3,6};/;
+let invertedCSS = ''; //  resulting string
+
+// functions
+
+const hasMatch = ( string ) => !!string.match(hexValueRegex);
+
+const hexColorOpposite = ( hex ) => {
+  // let arrOfHex = hex.split(''); // assumes !hex.includes('#')
+
+  let arrOfHex = hex.split('');
+
+  // starts at 1 because assumes hex[0] === '#'
+  // ends at length - 1 because last char should be ';'
+  for ( let i = 1; i < arrOfHex.length - 1; i++ ) {
+    let currentIndex = hexIndexReference[arrOfHex[i].toUpperCase()];
+    arrOfHex[i] = hexValueArray[15 - currentIndex];
+  }
+
+  return arrOfHex.join('');
+}
+
+const recurseInvertCSS = ( string ) => {
+
+
+  if ( hasMatch( string ) ) {
+    const regexMatch      = string.match( hexValueRegex );
+    const indexAfterMatch = regexMatch.index + regexMatch[0].length;
+    const remainingCSS    = regexMatch.input.slice( indexAfterMatch );
+    const pre = regexMatch.input.slice( 0, regexMatch.index ) + hexColorOpposite( regexMatch[0] );
+
+    invertedCSS += pre;
+    return recurseInvertCSS( remainingCSS );
+  } else {
+    return string;
+  }
+}
+
+program
+  .version('0.1.0')
+  .arguments('<file>')
+  .action( function ( file ) {
+    fs.readFile(file, 'utf8', ( err, data ) => {
+      if (err) throw err;
+      recurseInvertCSS( data );
+      fs.writeFile(`inverted-${file}`, invertedCSS, (err) => {
+        if (err) throw err;
+        console.log(`Inverted stylesheet created: inverted-${file}.`)
+      });
+    });
+  })
+  .parse(process.argv)
